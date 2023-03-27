79a7bceb0a6498460aad053930e64c37646b511d
Anshul Rana <anshulrana2510@gmail.com>
2023-03-27 15:26:03 +0530

    Update Navbar.js
    removed About bar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -53,15 +53,6 @@ export class Navbar extends Component {
                   <Link
                     className="nav-link active"
                     aria-current="page"
-                    to="/about"
-                  >
-                    About
-                  </Link>{" "}
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link active"
-                    aria-current="page"
                     to="/business"
                   >
                     Business{" "}
